1541cc4b8eaa1703505e13a75eff033e768ffbae
Ryan Weingart <rmweingart5@gmail.com>
2019-07-06 12:05:27 -0400

    created game array with start and end functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,5 @@
 $("#start").on("click", function (){
-    $("#start").remove();
-    for (i = 0; i < questionsArray.length; i++) {
-        $(".container").append('<h2>' + questionsArray[i].question + '<h2>');
-        for (x = 0; x < questionsArray[i].answers.length; x++) {
-            $(".container").append("<input type='radio' name='question: "+i+"' value= '" + questionsArray[i].answers[x] + "'> "+ questionsArray[i].answers[x]);
-        }
-    }
+   game.start();
 });
 
 var questionsArray = [{
@@ -38,3 +32,37 @@ var questionsArray = [{
     correctAnswer:"a",
 
 }];
+
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 60,
+    countDown: function (){
+        game.counter--;
+        $("#counter").html(game.counter);
+        if (game.counter === 0) {
+            console.log("time up");
+            game.done();
+        }
+    },
+    start: function (){
+        timer = setInterval (game.countDown, 1000);
+        $(".container").prepend('<h2>Time Remaining: <span id="counter">60</span> Seconds</h2>');
+        $("#start").remove();
+        for (i = 0; i < questionsArray.length; i++) {
+            $(".container").append('<h2>' + questionsArray[i].question + '<h2>');
+            for (x = 0; x < questionsArray[i].answers.length; x++) {
+                $(".container").append("<input type='radio' name='question: "+i+"' value= '" + questionsArray[i].answers[x] + "'> "+ questionsArray[i].answers[x]);
+            }
+        }
+    },
+    done: function () {
+        $.each($('input[name= "question: 0]" :checked'), function (){
+            if ($(this).val() === questionsArray[0].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        })
+    }
+}
