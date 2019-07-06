89aad6cb83888557e6b8aa5a67ede746e3ed0b9f
Ryan Weingart <rmweingart5@gmail.com>
2019-07-06 11:41:18 -0400

    appended questions to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,40 @@
 $("#start").on("click", function (){
-console.log("hi");
+    $("#start").remove();
+    for (i = 0; i < questionsArray.length; i++) {
+        $(".container").append('<h2>' + questionsArray[i].question + '<h2>');
+        for (x = 0; x < questionsArray[i].answers.length; x++) {
+            $(".container").append("<input type='radio' name='question: "+i+"' value= '" + questionsArray[i].answers[x] + "'> "+ questionsArray[i].answers[x]);
+        }
+    }
 });
 
 var questionsArray = [{
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
 }, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
 }, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
 }, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
 }, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
 }, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
 }, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
-}, {
-    question:"",
-    answers: [],
-    correctAnswer:"",
+    question:"what is the answer",
+    answers: ["a", "b", "c", "d"],
+    correctAnswer:"a",
+
 }];
